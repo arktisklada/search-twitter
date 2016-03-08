97ae1d1130d32e6ae6db32af7cb15f3d72141893
arktisklada <mail@enorganik.com>
2016-03-07 23:26:53 -0600

    Implements memcached

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-private
+  private
 
   def client
     @client ||= Twitter::REST::Client.new do |config|
@@ -11,6 +11,13 @@ private
       config.consumer_secret = ENV['CONSUMER_SECRET']
       config.access_token = session['access_token']
       config.access_token_secret = session['access_token_secret']
+    end
+  end
+
+  def cache_fetch(*keys, &block)
+    cache_key = keys.join("/")
+    Rails.cache.fetch cache_key, expires_in: 5.minutes do
+      block.call
     end
   end
 end
--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,12 +1,20 @@
 class SearchController < ApplicationController
   def tweet
     @query = params[:query]
-    @results = client.search @query, result_type: "popular"
+    json = cache_fetch(:tweet, @query) do
+      client.search(@query, result_type: "popular").to_json
+    end
+
+    @results = JSON.parse json
   end
 
   def people
     @query = params[:query]
     @page = params.fetch(:page, 1).to_i
-    @results = client.user_search @query, page: @page
+    json = cache_fetch(:people, @query, @page) do
+      client.user_search(@query, page: @page).to_json
+    end
+
+    @results = JSON.parse json
   end
 end
