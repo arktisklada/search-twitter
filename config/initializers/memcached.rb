3b6fb529d9eaca6e54560ec663db6aca8cd4e248
arktisklada <mail@enorganik.com>
2016-03-07 23:27:59 -0600

    proper implementation of memcached

--- config/initializers/memcached.rb
+++
@@ -1,3 +0,0 @@
-if ENV["MEMCACHEDCLOUD_SERVERS"]
-  config.cache_store = :dalli_store, ENV["MEMCACHEDCLOUD_SERVERS"].split(','), { :username => ENV["MEMCACHEDCLOUD_USERNAME"], :password => ENV["MEMCACHEDCLOUD_PASSWORD"] }
-end
